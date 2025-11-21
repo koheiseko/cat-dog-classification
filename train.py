ae6932d4cc63ea94df4151133acd712309d24bd4
koheiseko <koheiseko20@gmail.com>
2025-11-21 14:31:24 -0300

    fix: ajusta detalhes no fluxo de treino em train.py

--- train.py
+++ train.py
@@ -1,20 +1,18 @@
 import torch
 import torch.nn as nn
 import torchmetrics
-from src.engine import *
-from src.data_setup import *
+from src.engine import train, evaluate
+from src.data_setup import get_train_data_transformed, get_test_data_transformed, get_data_loader, train_test_split, load_dataset
 from src.model import ResNetTransfer
 import os
 
 SEED = 42
-
 IMAGE_SIZE = 224
-
 LEARNING_RATE = 0.0001
-N_EPOCHS = 20
+N_EPOCHS = 10
 N_EPOCHS_FINE_TUNING = 5
 N_CLASSES = 2
-
+BATCH_SIZE = 32
 TRAINING_SIZE = 0.2
 
 
@@ -24,7 +22,7 @@ def main():
 
     print("[INFO] Iniciando o download dos dados")
     dataset_name = "tongpython/cat-and-dog"
-    # load_dataset(dataset_name, "data")
+    load_dataset(dataset_name, "data")
     print("[INFO] Os dados foram baixados!")
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -40,7 +38,7 @@ def main():
     test_data = get_test_data_transformed("data/test_set", IMAGE_SIZE, imagenet_mean, imagenet_std)
     train_data, valid_data = train_test_split(train_and_valid_data, TRAINING_SIZE)
 
-    train_loader, valid_loader, test_loader = get_data_loader(train_data, valid_data, test_data, 32, True)
+    train_loader, valid_loader, test_loader = get_data_loader(train_data, valid_data, test_data, BATCH_SIZE, True)
 
     print("[INFO] Inicializando o modelo")
     model = ResNetTransfer(n_classes=N_CLASSES)
@@ -51,7 +49,7 @@ def main():
     accuracy = torchmetrics.Accuracy(task='multiclass', num_classes=N_CLASSES).to(device)
 
     print(f"[INFO] Iniciando o treinamento do modelo por {N_EPOCHS} épocas")
-    history = train(model, optimizer, xentropy, accuracy, train_loader, valid_loader, 20, device)
+    history = train(model, optimizer, xentropy, accuracy, train_loader, valid_loader, N_EPOCHS, device)
 
     print(f"[INFO] Iniciando o processo de avaliação nos dados de teste")
     model_acc = evaluate(model, test_loader, accuracy, device).compute().item()
@@ -63,14 +61,13 @@ def main():
 
     print("[INFO] Inicializando o fine tuning do modelo")
     for param in model.parameters():
-        param.require_grad = True
+        param.requires_grad = True
 
     learning_rate_fine_tune = LEARNING_RATE / 1000
-    n_epochs_fine_tuning = 5
 
     optimizer_fine_tune = torch.optim.AdamW(params=model.parameters(), lr=learning_rate_fine_tune)
 
-    history_fine_tune = train(model, optimizer_fine_tune, xentropy, accuracy, train_loader, valid_loader, n_epochs_fine_tuning, device)
+    history_fine_tune = train(model, optimizer_fine_tune, xentropy, accuracy, train_loader, valid_loader, N_EPOCHS_FINE_TUNING, device)
     print("[INFO] Fine tuning finalizado")
 
     torch.save(model.state_dict(), "models/resnet_finetuned.pth")
